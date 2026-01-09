efdd779921ab0fd9b764a68c6a1574cba6cf07cd
Alex Shpurov <alextmn1@gmail.com>
2026-01-09 10:16:10 -0500

    Refactor vesting logic and update environment variables
    - Updated the vesting component to conditionally display the status section based on initialization needs.
    - Modified the VestingService to handle allocation data more robustly, ensuring compatibility with updated contract methods.
    - Changed environment variables for vesting and coin addresses to new values for production and development environments.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  vestingContractAddress: '0x3B1f928a90AF6Bfbf3672c9ed4267a161bBB947C',
-  coinAddress: '0x948716f8592E5fb3036f6e9b0C463BC2ADb0Ce51',
+  vestingContractAddress: '0xF0497a44ECE2D5f7f4a442766e4941a8D544A584',
+  coinAddress: '0x83A9c9e0dB0f82d91B86f4c3539c28983CDc393f',
   coinSymbol: 'QONE',
   supabaseRestUrl: 'https://uvqnyufffifclezqkmzh.supabase.co/functions/v1/proof-by-address',
   walletConnectProjectId: '051e4ff495bf3f36a63792e87bf4ba7e',
