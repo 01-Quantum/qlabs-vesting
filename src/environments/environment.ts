46d6b281714af4339d95bbc196ba135bee4802df
Alex Shpurov <alextmn1@gmail.com>
2026-02-09 15:00:15 -0400

    Update environment variables for vesting contract and coin addresses, and adjust network details for production and development environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,19 +1,19 @@
 export const environment = {
   production: false,
-  vestingContractAddress: '0x1f613cA2aDDD27c6CC43a7355566F7a1852019a5',
-  coinAddress: '0x274f901C018ffa7d0705fFc47cEcc803857fE9c3',
+  vestingContractAddress: '0x014aD207Ad0deFbc6992f5F421263F4A4c223cb6',
+  coinAddress: '0x4F9E014f620D83b08342C8BDFf3043fb2220b727',
   coinSymbol: 'QONE',
   supabaseRestUrl: 'https://uvqnyufffifclezqkmzh.supabase.co/functions/v1/proof-by-address',
   walletConnectProjectId: '051e4ff495bf3f36a63792e87bf4ba7e',
   networkDetails: {
-    chainId: '0x3E6',
+    chainId: '0x3E7',
     chainName: 'Hyperliquid TestNet',
     nativeCurrency: {
       name: 'HYPE',
       symbol: 'HYPE',
       decimals: 18,
     },
-    rpcUrls: ['https://rpc.hyperliquid-testnet.xyz/evm'],
+    rpcUrls: ['https://rpc.hyperliquid.xyz/evm'],
     blockExplorerUrls: ['https://testnet.purrsec.com/'],
   }
 };
